1cd57683cf3322f9b78a7be19af1c7019f84c630
Agata <twardowska123@gmail.com>
2018-07-16 20:05:38 +0200

    poprawione

--- src/Project.java
+++ src/Project.java
@@ -5,22 +5,32 @@ public class Project {
     public static void main(String[] args) {
 
 
+        Scanner scan = new Scanner(System.in);
 
-    Scanner scan = new Scanner(System.in);
-        System.out.println("Podaj liczbę");
+        System.out.println("Podaj rozmiar tablicy");
         int size = scan.nextInt();
         double[] numbers = new double[size];
 
 
-        for (int i = 0; i < numbers.length ; i++) {
-            System.out.println("Podaj liczbę zmiennoprzecinkową");
-            double a = scan.nextDouble();
-            numbers[i] = a;
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Podaj liczbę");
+            numbers[i] = scan.nextDouble();
+
+
+        double sum = 0;
+
+            for (int i1 = 0; i1 < numbers.length; i1++) {
+                sum = sum + Math.pow(numbers[i],2);
+            }
         }
 
-            
-        }
+        System.out.println(sum);
+
     }
+
+}
+
+
 
 
 
